e9840fc5ba40a0b38924fd943d5dd45d9503d95a
nuatmochoi <tjddn8770@naver.com>
2020-01-06 22:06:15 +0900

    movie app made with ReactJS

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,13 @@
 import React from "react";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import "./Navigation.css";
 
 function Navigation(){
-    return <div>
+    return (<div className="nav">
         <Link to="/">Home</Link> 
         <Link to="/about">About</Link>
-    </div> // instead of <a>, use Link => it doesn't kill react
+    </div>
+    ); // instead of <a>, use Link => it doesn't kill react
 }
 
-export default Navigation
+export default Navigation;
